7a950c9f8f00230f9005137d36cc02b4af4153dc
Lvova Alina <lvova.aline@gmail.com>
2023-06-15 01:26:05 +0400

    fix: check response

--- controllers/users.js
+++ controllers/users.js
@@ -9,20 +9,22 @@ const BadRequestError = require('../errors/badRequestError');
 
 // Формат данных пользователя
 const formatUserData = (user) => ({
-  name: user.name,
   _id: user._id,
+  name: user.name,
   email: user.email,
 });
 
 module.exports.getMe = (req, res, next) => {
-  User.findById(req.user._id)
+  const userId = '648a23ebcf8216756f8be6a4'; // req.user._id;
+  User.findById(userId)
     .then((user) => res.status(SUCCESS_STATUS).send(formatUserData(user)))
     .catch((err) => next(err));
 };
 
 module.exports.updateUser = (req, res, next) => {
   const { name, email, password } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, email, password }, {
+  const userId = '648a23ebcf8216756f8be6a4'; // req.user._id;
+  User.findByIdAndUpdate(userId, { name, email, password }, {
     new: true,
     runValidators: true,
   })
