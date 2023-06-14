67d755f6471c776650d635fb5c6babaa64e9aae3
Lvova Alina <lvova.aline@gmail.com>
2023-06-14 23:40:51 +0400

    feature: add constant statuses

---
+++ utils/constants.js
@@ -0,0 +1,12 @@
+// Default error.
+const INTERNAL_SERVER_ERROR = 500;
+// Success - request processed successfully.
+const SUCCESS_STATUS = 200;
+// Success - new resource created.
+const CREATED_STATUS = 201;
+
+module.exports = {
+  INTERNAL_SERVER_ERROR,
+  SUCCESS_STATUS,
+  CREATED_STATUS,
+};
