eebf751bbef148a905ba1d0298ed7f3d20cb7e3f
Lvova Alina <lvova.aline@gmail.com>
2023-06-14 23:22:46 +0400

    feature: connect the database, configure express

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const { DB_ADDRESS } = require('./utils/config');
+
+// подключаемся к серверу mongo
+mongoose
+  .connect(DB_ADDRESS, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Успешное подключение к базе данных');
+  })
+  .catch((error) => {
+    console.log('Ошибка при подключении к базе данных:', error.name);
+  });
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+// теперь клиент имеет доступ только к публичным файлам
+app.use(express.static(path.join(__dirname, 'public')));
+
+// middleware для обработки данных в формате JSON
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(require('./routes/index'));
+
+app.listen(PORT, () => {
+});
