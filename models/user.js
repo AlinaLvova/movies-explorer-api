b831df9b2c8b6cfab5619b6aff01f5e0cd48add5
Lvova Alina <lvova.aline@gmail.com>
2023-06-14 16:44:11 +0400

    feature: add userShema

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  email: {
+    required: true,
+    type: String,
+    validate: {
+      validator: (email) => validator.isEmail(email),
+      message: 'Некорректный e-mail',
+    },
+    unique: true,
+  },
+  password: {
+    required: true,
+    type: String,
+    select: false,
+  },
+}, {
+  versionKey: false, // Отключение опции versionKey
+});
+
+module.exports = mongoose.model('user', userSchema);
