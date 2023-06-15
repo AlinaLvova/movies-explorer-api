4b6115084d029593032ded8c8fd2ed84df256006
Lvova Alina <lvova.aline@gmail.com>
2023-06-15 17:46:55 +0400

    feature: add signup router

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/user');
 const {
   SUCCESS_STATUS,
+  CREATED_STATUS,
 } = require('../utils/constants');
 
 const BadRequestError = require('../errors/badRequestError');
+const ConflictError = require('../errors/badRequestError');
 
 // Формат данных пользователя
 const formatUserData = (user) => ({
@@ -13,6 +16,31 @@ const formatUserData = (user) => ({
   name: user.name,
   email: user.email,
 });
+
+module.exports.createUser = (req, res, next) => {
+  const {
+    name,
+    email,
+    password,
+  } = req.body;
+
+  bcrypt.hash(password, 10).then((hash) => {
+    User.create({
+      name, email, password: hash,
+    })
+      .then((user) => res.status(CREATED_STATUS).send(formatUserData(user)))
+      .catch((err) => {
+        if (err instanceof mongoose.Error.ValidationError) {
+          return next(new BadRequestError('Переданы некорректные данные при создании пользователя.'));
+        }
+        if (err.code === 11000) {
+          return next(new ConflictError('Пользователь с таким email уже зарегистрирован'));
+        }
+        return next(err);
+      })
+      .catch(next);
+  });
+};
 
 module.exports.getMe = (req, res, next) => {
   const userId = '648a23ebcf8216756f8be6a4'; // req.user._id;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const usersRouter = require('./users');
 const moviesRouter = require('./movies');
+const { createUser } = require('../controllers/users');
 
 const router = express.Router();
+
+router.post('/signup', createUser);
 
 router.use('/users', usersRouter);
 router.use('/movies', moviesRouter);
