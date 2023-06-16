6fb388e6465587be66e8eeedf07564596cdf86f7
Lvova Alina <lvova.aline@gmail.com>
2023-06-16 17:59:35 +0400

    feature: add loggers

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const usersRouter = require('./users');
 const moviesRouter = require('./movies');
 const { createUser, login, logout } = require('../controllers/users');
 const { loginValidator, createUserValidator } = require('../middlewares/validators');
+const { requestLogger, errorLogger } = require('../middlewares/logger');
 const auth = require('../middlewares/auth');
 const NotFoundError = require('../errors/notFoundError');
 
@@ -14,6 +15,8 @@ router.post('/signin', loginValidator, login);
 router.post('/signup', createUserValidator, createUser);
 router.post('/signout', logout);
 
+router.use(requestLogger); // логгер запросов
+
 router.use(auth);
 
 router.use('/users', usersRouter);
@@ -21,6 +24,8 @@ router.use('/movies', moviesRouter);
 
 // Middleware для обработки несуществующих путей
 router.use((req, res, next) => next(new NotFoundError('Маршрут не найден')));
+
+router.use(errorLogger); // логгер ошибок
 
 router.use(handleErrorsByCelebrate()); // обработчик ошибок celebrate
 
