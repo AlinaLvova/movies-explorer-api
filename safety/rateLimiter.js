e1e86f25e250c498ae9519765c539326976c5950
AlinaLvova <lvova.aline@gmail.com>
2023-07-23 21:36:30 +0000

    fix: fix limits count

--- safety/rateLimiter.js
+++ safety/rateLimiter.js
@@ -5,7 +5,7 @@ const { RATE_LIMIT_EXCEEDED } = require('../utils/constants');
 // Конфигурация rate limiter
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 60 минут
-  max: 100, // Максимальное число запросов с одного IP за указанный период времени
+  max: 1000, // Максимальное число запросов с одного IP за указанный период времени
   message: RATE_LIMIT_EXCEEDED,
 });
 
